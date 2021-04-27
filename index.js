680cc7f84220f56fb033ce89175838642f5e31f4
abhi-192 <81365470+abhi-192@users.noreply.github.com>
2021-04-27 21:10:59 +0530

    main home page set up

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const port = 8000;
 
 const app = express();
 
+//set up ejs as view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.use('/',require('./routes'));
 
+// listening to port 8000
 app.listen(port,function(err){
     if(err){
         console;log("ERROR: in runnning the server");
