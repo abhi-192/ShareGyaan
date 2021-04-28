e64a6336214aa6fdb681e17d221168e88effc832
abhi-192 <81365470+abhi-192@users.noreply.github.com>
2021-04-28 11:38:18 +0530

    add functionality to ask question and view questions by particular user

--- controllers/question_controller.js
+++ controllers/question_controller.js
@@ -1,7 +1,8 @@
-const Question = require("../models/question")
+const Question = require("../models/question");
+const User = require("../models/user");
 
 module.exports.all = function(req,res){
-    Question.find({},function(err,Question){
+    Question.find({},function(err){
         if(err){
             console.log("ERROR : unable to send all question list");
             return;
@@ -9,3 +10,43 @@ module.exports.all = function(req,res){
         return res.render('question');
     });
 }
+
+module.exports.ask  =function(req,res){
+    //chack if user is logged in or not
+
+    Question.find({content: req.body.content} ,function(err,question){
+        if(err){
+            console.log("ERROR: unable to search question");
+            return;
+        }
+        if(!question){
+
+            //adding a question to db if it doesnt exist.
+            Question.create(req.body,function(err, question){
+                if(err)
+                {
+                    console.log("ERROR: unable create new queestion");
+                    return;
+                }
+                console.log("Question created succesfully");
+                User.findById({_id: question.user_id},function(err,user){
+                    if(err){
+                        console.log('Unable to find user.');
+                        return ;
+                    }
+                    user.question_id.push(_id);
+                });
+                res.redirect('./question/question._id');
+            });
+        }
+    });
+}
+
+module.exports.me = function(req,res){
+
+    ///redirect to './users/questions/__'
+
+    let path = './users/questions/' + req.body._id;
+    res.redirect(path);
+}
+
